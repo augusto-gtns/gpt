98a62a8875e7237998885aa2b0edc0c5d7736917
augusto.celso.souza <augusto.souza@nava.com.br>
2023-04-03 14:36:12 -0300

    feat: add support to code and shel generation

--- README.md
+++ README.md
@@ -1,9 +1,124 @@
-# Use GPT3 on terminal
+# GPT3 script
 
-TL;DR
+Shell script to interact with GPT3 on your terminal.
 
-![TLDR](TLDR.png)
+## Installation
+--- 
 
-Add an alias
+Clone this repo or download the `gpt3.sh` script.
 
-`alias gpt3="~/gpt3.sh"`
+```bash
+git clone https://github.com/augusto-gtns/gpt3.git
+```
+
+Generate and set your personal API key (https://platform.openai.com/account/api-keys)
+
+```bash
+export OPENAI_API_KEY=mykey
+```
+
+Set execution permission.
+
+```bash
+chmod +x gpt3.sh
+```
+
+Run the script on your terminal
+
+- call the script
+    
+    ```bash
+    ./gpt3.sh
+    ```
+
+- Or map an alias on your shel configuration (`.bashrc`, `.zshrc` or others) and call the given alias
+
+    ```bash
+    alias gpt3="~/gpt3.sh"
+    ```
+     
+    ```bash
+    gpt3
+    ```
+
+## Usage
+--- 
+
+### Prompt once 
+
+**[prompt]**
+
+```bash
+gpt3 what is the top 3 most popular programing languages
+```
+
+### Start a new chat session 
+
+**[--chat|-c] [assitant-role]**
+
+> The a assitant role is used to tell the chat the which role should be act.
+
+- Using the default assitant role
+  
+    ```bash
+    gpt3 --chat
+    ```
+
+- Suplly a custom assitant role
+  
+    ```bash
+    gpt3 -c You are a tech expert
+    ```
+
+### Generate shell commands
+
+**[--shell|-s] [prompt]**
+
+```bash
+gpt3 --shell
+```
+
+```bash
+gpt3 -s list the name path and size of the 5 biggest files on my machine
+```
+
+### Generate code
+
+**[--code|-C] [lang]**
+
+```bash
+gpt3 --code
+```
+
+```bash
+gpt3 -C java
+```
+
+### See the usage heper:
+
+**[--help|-h]**
+
+```bash
+gpt3 -h
+```
+
+```bash
+gpt3 --help
+```
+
+## Configuration
+
+Available envinroment variables. Run `gpt3 --help` to check the default values.
+
+| Var                 | Description                                                                                                                                                            |
+| ------------------- | ---------------------------------------------------------------------------------------------------------------------------------------------------------------------- |
+| OPENAI_API_KEY      | Your personal API key.                                                                                                                                                 |
+| OPENAI_TEMPERATURE  | Default temperature for API calls.<br /> higher values make the output more random, lower values like make it more focused and deterministic [values between 0 and 2]. |
+| OPENAI_CHAT_ROLE    | Default assistant role to start a new chat sessions.                                                                                                                   |
+| OPENAI_SHELL_PREFIX | Default prefix to --shell prompts.                                                                                                                                     |
+| OPENAI_CODE_PREFIX  | Default prefix to --code prompts.                                                                                                                                      |
+> The a assitant role is used to tell the chat the which role should be act.
+## Reference
+---
+
+- [OpenAI API docs](https://platform.openai.com/docs/api-reference)
--- gpt3.sh
+++ gpt3.sh
@@ -5,16 +5,19 @@
 ###
 
 if [ "$OPENAI_API_KEY" == "" ]; then
-	printf "\n - please set OPENAI_API_KEY env var (export OPENAI_API_KEY=mykey) \n"
+	printf "\n - please set OPENAI_API_KEY env var \n"
 	exit
 fi
 
-# Initial message for chat
-[[ -z "${OPENAI_DEFAULT_ROLE}" ]] && OPENAI_DEFAULT_ROLE="You are a helpful assistant"
+[[ -z "${OPENAI_TEMPERATURE}" ]] && OPENAI_TEMPERATURE=0.5
 
-# What sampling temperature to use, between 0 and 2. 
-# Higher values like 0.8 will make the output more random, while lower values like 0.2 will make it more focused and deterministic.
-[[ -z "${OPENAI_DEFAULT_TEMPERATURE}" ]] && OPENAI_DEFAULT_TEMPERATURE=0.5
+[[ -z "${OPENAI_MAXTOKENS}" ]] && OPENAI_MAXTOKENS=500
+
+[[ -z "${OPENAI_CHAT_ROLE}" ]] && OPENAI_CHAT_ROLE="You are a helpful assistant."
+
+[[ -z "${OPENAI_SHELL_PREFIX}" ]] && OPENAI_SHELL_PREFIX="Generate a shell command compatible with the operating system [os] and that responds to [prompt]. Add a brief explanation of the generated command."
+
+[[ -z "${OPENAI_CODE_PREFIX}" ]] && OPENAI_CODE_PREFIX="Generate code for the language [lang] and that responds to the [prompt]. Add a brief explanation of the generated code."
 
 ###
 # functions
@@ -22,113 +25,83 @@ fi
 
 display_usage(){
 	echo "
-1) Prompt once using completion API (https://platform.openai.com/docs/api-reference/completions) 
+
+See https://github.com/augusto-gtns/gpt3.
+
+Envinroment configuration values:
+
+	OPENAI_API_KEY: ******
 	
-	<prompt>
+	OPENAI_TEMPERATURE: $OPENAI_TEMPERATURE
 
-	gpt3 how to install wget on linux
-	gpt3 \"what is the top 3 most popular programing languages?\"
+	OPENAI_MAXTOKENS: $OPENAI_MAXTOKENS
 
-2) Starts a new session using chat API (https://platform.openai.com/docs/api-reference/chat)
+	OPENAI_CHAT_ROLE: $OPENAI_CHAT_ROLE
 
-	[--chat|-c] <assitant role>
-
-	gpt3 -c
-	gpt3 -c You are a tech expert 
-
-3) See this usage heper
+	OPENAI_SHELL_PREFIX: $OPENAI_SHELL_PREFIX
 	
-	[--help|-h|]
-
-	gpt3 -h
-
-4) Envinroment configurations:
-
-	- OPENAI_API_KEY: ******
-
-	  Your personal API key.
-
-
-	- OPENAI_DEFAULT_TEMPERATURE: $OPENAI_DEFAULT_TEMPERATURE
-
-	  Default temperature for API calls.
-
-
-	- OPENAI_DEFAULT_ROLE: $OPENAI_DEFAULT_ROLE
-
-	  Default initial assistant message to start a new chat session.
-	  A custom message can be used by supplying --chat option.
-	  Used to tell the chat the which role should be act.
-"
+	OPENAI_CODE_PREFIX: $OPENAI_CODE_PREFIX
+	"
 }
 
 quick_prompt(){
+	prompt="$@"
 	
 	payload='{
 		"model": "text-davinci-003",
-		"temperature": '$OPENAI_DEFAULT_TEMPERATURE',
-		"max_tokens": 500,
+		"temperature": '$OPENAI_TEMPERATURE',
+		"max_tokens": '$OPENAI_MAXTOKENS',
 		"prompt": "'$prompt'"
 	}'
-	# echo "payload: $payload"
 	
 	response=$(curl https://api.openai.com/v1/completions \
 		-H "Content-Type: application/json" \
 		-H "Authorization: Bearer $OPENAI_API_KEY" \
 		-d "$payload" \
-		-k -L -s --connect-timeout 5 \
-	)
-	# echo "response: $response"
-	
+		-k -L -s --connect-timeout 5)
+
 	answer=$(jq -r '.choices[].text' <<< "${response}")	
 	printf "$answer \n"
 }
 
 start_chat(){
+	role="$@"
 
 	if [ "$role" == "" ]; then
-		role=$OPENAI_DEFAULT_ROLE
+		role=$OPENAI_CHAT_ROLE
 	fi
-	printf "\n- assistant role: $role\n\n"
+	printf "\nassistant role: $role\n\n"
 
 	payload='{
 		"model": "gpt-3.5-turbo",
-		"temperature": '$OPENAI_DEFAULT_TEMPERATURE',
+		"temperature": '$OPENAI_TEMPERATURE',
 		"messages": [
 			{"role": "assistant", "content": "'$role'"}
 		]
 	}'
-	# echo $payload
 
 	while [ true ]; do
 
 		while [ "$prompt" == "" ]; do
 			read -p "you: " prompt
 		done
-		# echo "prompt: $prompt"
 
 		user_message='{"role": "user", "content": "'$prompt'"}'
-		# echo "user_message: $user_message"		
 
 		payload=$(echo $payload | jq ".messages[.messages| length] |= . + $user_message")
-		# echo "payload: $payload"		
 
 		response=$(curl https://api.openai.com/v1/chat/completions \
 			-H "Content-Type: application/json" \
 			-H "Authorization: Bearer $OPENAI_API_KEY" \
 			-d "$payload" \
-			-k -L -s --connect-timeout 10 \
-		)
-		# echo "$response"
+			-k -L -s --connect-timeout 10)
 		
 		answer=$(jq -r '.choices[].message.content' <<< "${response}")	
 		printf "\n $answer \n\n"
 
 		assistant_message='{"role": "assistant", "content": "'$answer'"}'
-		# echo "assistant_message: $assistant_message"
 
 		payload=$(echo $payload | jq ".messages[.messages| length] |= . + $assistant_message")
-		# echo "payload: $payload"		
 
 		prompt=""
 	done
@@ -139,21 +112,36 @@ start_chat(){
 ###
 
 if [[ ( $1 == "--help") ||  $1 == "-h" ||  $@ == "" ]]; then 
-	# echo "display_usage"	
 	display_usage
 
-elif [[ ( $1 == "--chat") ||  $1 == "-c" ]]; then 
-	# echo "start_chat"
-	
-	role="${@:2}"
-	# echo "role: $role"
-	
-	start_chat
-else
-	# echo "quick_prompt"
+elif [[ ( $1 == "--chat") ||  $1 == "-c" ]]; then 	
+	start_chat "${@:2}"
 
-	prompt="$@"
-	# echo "prompt: $prompt"
+elif [[ ( $1 == "--code") ||  $1 == "-C" ]]; then 
 	
-	quick_prompt
+	lang="$2"
+	while [ "$lang" == "" ]; do
+		read -p "language: " lang
+	done
+
+	while [ "$prompt" == "" ]; do
+		read -p "prompt: " prompt
+	done
+
+	quick_prompt "$OPENAI_CODE_PREFIX \n\n [lang]: $lang \n\n [prompt]: $prompt"
+
+elif [[ ( $1 == "--shell") ||  $1 == "-s" ]]; then 
+	
+	source /etc/lsb-release
+	my_os="$(uname) $(echo $DISTRIB_ID) $(echo $DISTRIB_RELEASE)"	
+
+	prompt="${@:2}"
+	while [ "$prompt" == "" ]; do
+		read -p "prompt: " prompt
+	done
+
+	quick_prompt "$OPENAI_SHELL_PREFIX \n\n [os]: $my_os \n\n [prompt]: $prompt"
+
+else	
+	quick_prompt "$@"
 fi
